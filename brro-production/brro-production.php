18d7ccf434874af06baf63382dbf134a8881b8f5
ronaldpostma <admin@ronaldpostma.com>
2024-03-20 10:26:27 +0100

    Base structure of the plugin

---
+++ brro-production/brro-production.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Brro Production Files
+ * Description: Brro website production style, script and functions
+ * Version: 1
+ * Author: Ronald Postma 
+ * Author URI: https://brro.nl/
+ * 
+ */
+//
+// Include php functions file
+// 
+require_once plugin_dir_path(__FILE__) . '/brro-production-functions.php';
+//
+// Load script
+//
+add_action( 'wp_enqueue_scripts', 'brro_enqueue_production_script' );
+function brro_enqueue_production_script() {
+    wp_enqueue_script( 'brro-frontend-inspector-script', plugins_url( 'brro-production-script.js', __FILE__ ), [ 'jquery' ], '1.0.0', true );
+}
+//
+// Load CSS
+//
+add_action( 'wp_enqueue_scripts', 'brro_enqueue_production_css' );
+function brro_enqueue_production_css() {
+    // Front-end CSS file for live website
+    $version = get_option('brro_production_vars_version', '1.0.0'); // Renews each time the file is regenerated. Defaults to '1.0.0' if nothing is set
+    wp_enqueue_style( 'brro-production-vars', plugins_url( '/css/brro-production-vars.css', __FILE__ ), [], $version);
+}
