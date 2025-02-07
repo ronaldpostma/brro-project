80c0701022844ddb0fff230b5003bfd68e52a07e
ronaldpostma <admin@ronaldpostma.com>
2025-02-07 12:13:25 +0100

    max length excerpt update

--- brro-project/js/brro-project-wp-admin-script.js
+++ brro-project/js/brro-project-wp-admin-script.js
@@ -4,11 +4,16 @@ jQuery(function($) {
     // Limit excerpt to 141 characters
     var maxLength = 141;
     var excerptText = $('#excerpt');
-	excerptText.attr('maxlength', maxLength);
+    $('textarea#excerpt + p').addClass('cust-excerpt').text('Wordt gebruikt als samenvatting en meta-beschrijving voor zoekmachines. Max ' + maxLength + ' karakters');
+    excerptText.attr('maxlength', maxLength);
     excerptText.on('input', function() {
         var text = excerptText.val();
         if (text.length > maxLength) {
             excerptText.val(text.substring(0, maxLength));
         }
+        $('textarea#excerpt').on('input', function() {
+            var text = $(this).val();
+            $('textarea#excerpt + p').text('Wordt gebruikt als samenvatting en meta-beschrijving voor zoekmachines. Max ' + maxLength + ' karakters: ' + text.length + '/' + maxLength);
+        });
     });
 });
