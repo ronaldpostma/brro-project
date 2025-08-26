af67e3c7ff2f9422f3d8ea027f88991f7f35a3e2
ronaldpostma <admin@ronaldpostma.com>
2025-08-26 16:09:14 +0200

    versions

--- brro-project/brro-project.php
+++ brro-project/brro-project.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: Brro -PROJECT NAME- Custom Code
  * Description: Custom style, script and functions for - PROJECT NAME -
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: Ronald Postma 
  * Author URI: https://brro.nl/
  * 
  */
 if (!defined('ABSPATH')) exit;
 //
-// Plugin retired - use brro-core and brro-flex theme instead or check versions in zip folder
+// Plugin retired - use brro-core and brro-flex theme instead or use version 1.1.3 in zip folder
